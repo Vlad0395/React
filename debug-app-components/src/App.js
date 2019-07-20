4ce14481448d4af833e814ebc805750c5fcb07c2
admasutphp <vlad2009_@ukr.net>
2019-07-20 10:06:55 +0300

    Edit indents

--- debug-app-components/src/App.js
+++ debug-app-components/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       isLoggedIn: false
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>You are currently logged {this.state.isLoggedIn === true? 'in':'out'}</h1>
+        <h1>You are currently logged {this.state.isLoggedIn === true ? 'in' : 'out'}</h1>
       </div>
     )
   }
