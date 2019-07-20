5175d7032245117c7a2b1533e1f5b53015d0206a
admasutphp <vlad2009_@ukr.net>
2019-07-20 09:49:29 +0300

    after debaging

--- debug-app-components/src/App.js
+++ debug-app-components/src/App.js
@@ -4,15 +4,18 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      name: 'Vasya',
-      age : 45
+      name: 'Vlad',
+      age : 24,
+      hobbi: 'anythink'
     }
   }
+
   render() {
     return (
       <div>
         <h1>{this.state.name}</h1>
         <h3><font color="#3AC1EF">▍{this.state.age} years old</font></h3>
+        <h6><font color="#3AC1EF">▍{this.state.hobbi} cool</font></h6>
       </div>
     )
   }
