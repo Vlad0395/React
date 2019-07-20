48bf66e07df3fd586e5d18d1430d0c1d70f70783
admasutphp <vlad2009_@ukr.net>
2019-07-20 09:44:37 +0300

    What was before debuging code

---
+++ debug-app-components/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'Vasya',
+      age : 45
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h3><font color="#3AC1EF">▍{this.state.age} years old</font></h3>
+      </div>
+    )
+  }
+}
+
+export default App
