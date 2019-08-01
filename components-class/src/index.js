cdf57f5f55f9c30b3f73edae84b217cf9efbec8c
Vlad <37487589+Vlad0395@users.noreply.github.com>
2019-08-01 23:30:47 +0300

    debugging all code and create new project for dubaggin code in new tasc

--- components-class/src/index.js
+++ components-class/src/index.js
@@ -1,21 +1,47 @@
-import React,{Component} from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom"
 
-class App extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            answer: "Yes"
-        }
-    }
+// #1
+class App extends Component {
     render() {
         return (
             <div>
-                <h1>Is state important to know?{this.state.answer}</h1>
+                <Header username = 'vschool' />
+                <Greeting />
             </div>
         )
     }
 }
-// Constructor()
+
+// #2
+class Header extends Component {
+    render(){
+    return(
+        <header >
+            <p>Welcome, {this.props.username}!</p>
+        </header >
+    )
+    }
+}
+
+// #3
+class Greeting extends Component {
+    render(){
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+    
+        if (hours < 12) {
+            timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+        return (
+            <h1>Good {timeOfDay} to you, sir or madam!</h1>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"))
