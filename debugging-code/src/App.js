43919e23e6d11dbe5be4aa23fd0054b3755e20e5
Vlad <37487589+Vlad0395@users.noreply.github.com>
2019-08-04 12:58:30 +0300

    added in project 'todo' function handleChange for switch checkbox and other

--- debugging-code/src/App.js
+++ debugging-code/src/App.js
@@ -6,16 +6,23 @@ class App extends Component {
         super()
         this.state = {
             name: 'Vlad',
-            age: '24',
+            age: 24,
             answer: false
         }
     }
     render() {
         return (
+            // <div>
+            //     <h1>{this.state.name}</h1>
+            //     <h3><font color="#3AC1EF">▍{this.state.age} years old</font></h3>
+            //     <h1>You are currently logged {this.state.answer ? 'in':'out'}</h1>
+            // </div>
             <div>
-                <h1>{this.state.name}</h1>
-                <h3><font color="#3AC1EF">▍{this.state.age} years old</font></h3>
-                <h1>You are currently logged {this.state.answer ? 'in':'out'}</h1>
+                <img src="https://www.fillmurray.com/200/150" onMouseOver={()=>console.log('I was over')}/>
+                <br />
+                <span>I is text {this.state.age}</span>
+                <br />
+                <button onClick={()=>this.setState({age:this.state.age + 1})}>Click me</button>
             </div>
         )
     }
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,40 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import TodoItem from "./components/TodoItem"
 import todosData from "./components/todosData"
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      todos:todosData
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
-  }
-  render(){
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
-    
-    return (
-      <div className="todo-list">
-            {todoItems}
-        </div>
-    )
-  }
+    handleChange(id) {
+        // console.log('Chenged', id)
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id){
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return{
+                todos: updatedTodos
+            }
+        })
+    }
+
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
